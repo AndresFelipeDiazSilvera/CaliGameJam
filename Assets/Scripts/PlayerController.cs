8a05859b33b95c8c7e6cef932d130a51c2c2da3e
MrFackry <daniel.d.arcos05gmail.com>
2025-06-24 15:51:50 -0500

    feature: se creo el movimiento del player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,30 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float speed;
     [SerializeField] float windForce;
+    [SerializeField] PlayerInput playerInput;
+    public Vector2 inputs;
+    private Rigidbody2D rB2D;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rB2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        inputs = playerInput.actions["Move"].ReadValue<Vector2>();
+    }
+    void FixedUpdate()
+    {
+        Vector2 movimiento = inputs.normalized;
+        if (movimiento!=Vector2.zero)
+        {
+            rB2D.MovePosition(rB2D.position + speed * Time.fixedDeltaTime * movimiento);
+        }
     }
 }
