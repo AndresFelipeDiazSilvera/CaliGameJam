3bb2c7bbccdd61fd37a614f11affee70e838b51b
MrFackry <daniel.d.arcos05gmail.com>
2025-06-24 16:34:19 -0500

    feature: se creo el escrip para que la camara siga al player

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -2,15 +2,20 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
+    [SerializeField] GameObject player;
+    [SerializeField] private Vector3 offSet = new Vector3(0, 0, -10);
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (player != null)
+        {
+            transform.position = player.transform.position + offSet;
+        }
     }
 }
